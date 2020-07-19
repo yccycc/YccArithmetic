1d1fab7aeee27e05ada87d1f2fc7d65d98b1fe2e
NU03 <772878320@qq.com>
2020-07-19 11:06:13 +0800

    select sort

---
+++ app/src/main/java/com/example/yccarithmetic/YccSelectFinal.java
@@ -0,0 +1,26 @@
+package com.example.yccarithmetic;
+
+import java.util.Arrays;
+
+public class YccSelectFinal {
+    public static void main(String[] args) {
+        int a[] = {1000,2,1,5,3,9,8,6,7,4,10,10,7};
+        yccSelectSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+    public static void yccSelectSort(int a[]){
+        for (int i=0;i<a.length;i++){
+            int minIndex = i;
+            for (int j=i+1;j<a.length;j++){
+                if(a[j]<a[minIndex]){
+                    minIndex = j;
+                }
+            }
+            if(i != minIndex){
+                int temp = a[i];
+                a[i] = a[minIndex];
+                a[minIndex] = temp;
+            }
+        }
+    }
+}
